f45683e77f3abebcb839d4094c935775feb8b367
felipesartori <felipesartori.ti@gmail.com>
2022-04-27 15:10:18 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,101 +1,29 @@
-function changeRed() {
-    var element = document.getElementById("box");
-    element.style.backgroundColor = 'red';
+const asyncGetCall = async () => {
+    try {
+        const response = await fetch('https://arcane-stream-30681.herokuapp.com/state');
+        const data = await response.json();
+
+        var state = data[0].state
+        if (state === 'green') {
+            var element = document.getElementById("box");
+            element.style.backgroundColor = 'green';
+        } else if (state === 'red') {
+            var element = document.getElementById("box");
+            element.style.backgroundColor = 'red';
+        }
+    } catch (error) {
+        return error
+    }
 }
 
-function changeGreen() {
-    var element = document.getElementById("box");
-    element.style.backgroundColor = 'green';
+const asyncPostCall = async (color) => {
+    try {
+        await fetch('https://arcane-stream-30681.herokuapp.com/state', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ state: color })
+        });
+    } catch (error) {
+        return error
+    }
 }
-
-// fetch('http://example.com/api', {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json'}
-// });
-
-// const getState = async (color) => {
-//     await fetch('https://arcane-stream-30681.herokuapp.com/state'), {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({ state: color })
-//     }
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data)
-//             // var state = data[0].state
-//             // if (state === 'green') {
-//             //     var element = document.getElementById("box");
-//             //     element.style.backgroundColor = 'green';
-//             // } else if (state === 'red') {
-//             //     var element = document.getElementById("box");
-//             //     element.style.backgroundColor = 'red';
-//             // }
-//         })
-// }
-
-(async () => {
-    const rawResponse = await fetch('https://arcane-stream-30681.herokuapp.com/state', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ state: 'green' })
-    });
-    const content = await rawResponse.json();
-
-    console.log(content);
-})();
-
-
-// function onGet() {
-//     const url = "https://arcane-stream-30681.herokuapp.com/state";
-//     var headers = {}
-
-//     fetch(url, {
-//         method: "GET",
-//         mode: 'cors',
-//         headers: headers
-//     })
-//         .then((response) => {
-//             if (!response.ok) {
-//                 throw new Error(response.error)
-//             }
-//             console.log(response.json())
-//             return response.json();
-//         })
-//         .then(data => {
-//             console.log(data)
-//             // document.getElementById('messages').value = data.messages;
-//         })
-//         .catch(function (error) {
-//             console.log(error)
-//             // document.getElementById('messages').value = error;
-//         });
-// }
-// onGet()
-
-
-// function ler() {
-//     fetch('https://arcane-stream-30681.herokuapp.com/state')
-//         .then(response => response.text())
-//         .then(text => {
-//             const array = text.split("\n");
-//             console.log(array);
-//         })
-// }
-
-// function ler() {
-//     const url = 'https://arcane-stream-30681.herokuapp.com/state';
-//     fetch(url)
-//         .then((resp) => resp.json())
-//         .then(function (data) {
-//             let authors = data.results;
-//             console.log(authors)
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-// }
-// ler()
-
