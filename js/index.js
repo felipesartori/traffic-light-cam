d2f6f37eada6705ea91c198dbf50dd9a09280a7a
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-04-27 08:50:49 -0300

    Trecho js para mudar a cor

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function changeRed() {
+    var element = document.getElementById("box");
+    element.style.backgroundColor = 'red';
+}
+
+function changeGreen() {
+    var element = document.getElementById("box");
+    element.style.backgroundColor = 'green';
+}
